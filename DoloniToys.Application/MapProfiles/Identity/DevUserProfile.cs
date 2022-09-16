4db2cfe3578aeea4d8f241ca4b906dee33f7a6e9
HennessyZazzz <zalubovskijv94@gmail.com>
2022-09-16 12:24:15 +0300

    add MapProfiles

---
+++ DoloniToys.Application/MapProfiles/Identity/DevUserProfile.cs
@@ -0,0 +1,21 @@
+﻿using AutoMapper;
+using DoloniToys.Domain.Dtos.Identity;
+using DoloniToys.Domain.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DoloniToys.Application.MapProfiles.Identity
+{
+    public class DevUserProfile : Profile
+    {
+        public DevUserProfile()
+        {
+            CreateMap<DevUser, DevUserDto>()
+                .ForMember(dest => dest.Email, source => source.MapFrom(src => src.Email))
+                .ForMember(dest => dest.PhoneNumber, source => source.MapFrom(src => src.PhoneNumber));
+        }
+    }
+}
