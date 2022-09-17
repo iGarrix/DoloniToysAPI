49370628a6d1910beb65345fb9113de43c15ef0a
HennessyZazzz <zalubovskijv94@gmail.com>
2022-09-17 17:33:41 +0300

    update User

--- DoloniToys.Domain/Dtos/Identity/UserDto.cs
+++ DoloniToys.Domain/Dtos/Identity/UserDto.cs
@@ -10,15 +10,8 @@ namespace DoloniToys.Domain.Dtos.Identity
 {
     public class UserDto : BaseDtoModel
     {
-        public string Name { get; set; }
-        public string Surname { get; set; }
+        public string Username { get; set; }
         public string Email { get; set; }
         public string PhoneNumber { get; set; }
-        public string? Fax { get; set; }
-        public string? Company { get; set; }
-        public string Address { get; set; }
-        public string City { get; set; }
-        public string Country { get; set; }
-        public string Region { get; set; }
     }
 }
